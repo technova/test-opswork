2d484e7be7c0f39ea0a3a3d412a009d4eb35bc5e
technova <rajesh.soni@saviance.com>
2014-01-11 18:44:39 +0530

    Update deploy_app.rb
    Change2

--- catalog/recipes/deploy_app.rb
+++ catalog/recipes/deploy_app.rb
@@ -1,22 +1,22 @@
-node[:deploy].each do |application, deploy|
+node["deploy"].each do |application, deploy|
   application "#{application}" do
     path "/srv/www/#{application}"
     owner 'deploy'
     group 'www-data'
     scm_provider Chef::Provider::Subversion
-    repository deploy[:scm][:repository]
-    revision   deploy[:scm][:revision]
-    environment_name deploy[:rails_env]
+    repository deploy["scm"]["repository"]
+    revision   deploy["scm"]["revision"]
+    environment_name deploy["rails_env"]
     rails do
       bundler_deployment true
       # bundle_options "2>&1 | tee -a /var/log/bundle-out.log"
       database do
-        host deploy[:database][:host]
-        adapter deploy[:database][:adapter]
-        database deploy[:database][:database]
-        username deploy[:database][:username]
-        password deploy[:database][:password]
-        reconnect deploy[:database][:reconnect]
+        host deploy["database"]["host"]
+        adapter deploy["database"]["adapter"]
+        database deploy["database"]["database"]
+        username deploy["database"]["username"]
+        password deploy["database"]["password"]
+        reconnect deploy["database"]["reconnect"]
       end
       # database_master_role "redmine_database_master"
     end
@@ -30,7 +30,7 @@ node[:deploy].each do |application, deploy|
     # end
 
     passenger_apache2 do
-      server_aliases deploy[:domains]
+      server_aliases deploy["domains"]
       webapp_template "#{application}.conf.erb"
     end
   end
