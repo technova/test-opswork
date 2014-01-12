e9eb0983a18fb8c3a67a21382669319c948a11da
technova <rajesh.soni@saviance.com>
2014-01-12 09:53:21 +0530

    Update default.rb
    test

--- catalog/recipes/default.rb
+++ catalog/recipes/default.rb
@@ -1,50 +1,52 @@
 #
-# Cookbook Name:: simple-apache
+# Cookbook Name:: catalog
 # Recipe:: default
 #
 # Copyright 2014, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
-# bash "updates packages" do
-# 	code "apt-get update"
+bash "updates packages" do
+	code "apt-get update"
+end
+ 
+# apt-get install git
+package "git"
+package "subversion"
+package "imagemagick"
+package "libmagickwand-dev"
+
+# # Use apache.conf.erb with my attributes and variables to create the file below with correct permissions.
+# template "/etc/apache2/apache2.conf" do
+# source "apache2.conf.erb"
+# owner "root"
+# group "root"
+# mode "644"
+# end
+
+# template "/etc/apache2/mods-available/passenger.conf" do
+# source "passenger.conf.erb"
+# owner "root"
+# group "root"
+# mode "644"
+# end
+
+# template "/etc/apache2/mods-available/passenger.load" do
+# source "passenger.load.erb"
+# owner "root"
+# group "root"
+# mode "644"
 # end
  
-# # apt-get install git
-# package "git"
-# package "subversion"
-# package "apache2"
+# # Starts apache.
+# # Configures apache to start at boot.
+# service "apache2" do
+#   supports :restart => true, :reload => true
+#   action :enable
+# end
 
-# Use apache.conf.erb with my attributes and variables to create the file below with correct permissions.
-#template "/etc/apache2/apache2.conf" do
-#source "apache2.conf.erb"
-#owner "root"
-#group "root"
-#mode "644"
-#end
-
-#template "/etc/apache2/mods-available/passenger.conf" do
-#source "passenger.conf.erb"
-#owner "root"
-#group "root"
-#mode "644"
-#end
-
-#template "/etc/apache2/mods-available/passenger.load" do
-#source "passenger.load.erb"
-#owner "root"
-#group "root"
-#mode "644"
-#end
- 
-# Starts apache.
-# Configures apache to start at boot.
-#service "apache2" do
-#  supports :restart => true, :reload => true
-#  action :enable
-#end
-
-# include_recipe "apache2::mod_deflate"
-# include_recipe "rails::configure"
-# include_recipe "passenger_apache2"
-# include_recipe "passenger_apache2::mod_rails"
+include_recipe "ssh_users"
+include_recipe "apache2"
+include_recipe "apache2::mod_deflate"
+include_recipe "passenger_apache2"
+include_recipe "passenger_apache2::mod_rails"
