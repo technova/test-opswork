03368f7c9d33f0b661c3e493ae227494a9cc4232
Rajesh Soni <rajesh.soni@saviance.com>
2014-01-13 22:37:21 +0530

    rajesh

--- catalog/recipes/deploy_app.rb
+++ catalog/recipes/deploy_app.rb
@@ -1,22 +1,34 @@
+application="goldappnew"
+reconnect = true
+host = "testrdsnew.c5x3ackbzbcn.ap-southeast-1.rds.amazonaws.com"
+password = "m3tn4bm6rg"
+adapter = "mysql"
+username = "root"
+database = "iima_dev_migration_development"
+repository = "svn://61.16.234.115/gold/branches/gold1.2.2"
+user = "shreya"
+password = "saviance"
+revision = "HEAD"
+rails_env = "iima_ops"
 node[:deploy].each do |application, deploy|
   application "#{application}" do
     path "/srv/www/#{application}"
     owner 'deploy'
     group 'www-data'
     scm_provider Chef::Provider::Subversion
-    repository deploy[:scm][:repository]
-    revision   deploy[:scm][:revision]
-    environment_name deploy[:rails_env]
+    repository repository
+    revision   revision
+    environment_name rails_env
     rails do
       bundler_deployment true
       # bundle_options "2>&1 | tee -a /var/log/bundle-out.log"
       database do
-        host deploy[:database][:host]
-        adapter deploy[:database][:adapter]
-        database deploy[:database][:database]
-        username deploy[:database][:username]
-        password deploy[:database][:password]
-        reconnect deploy[:database][:reconnect]
+        host host
+        adapter adapter
+        database database
+        username username
+        password password
+        reconnect reconnect
       end
       # database_master_role "redmine_database_master"
     end
@@ -30,8 +42,8 @@ node[:deploy].each do |application, deploy|
     # end
 
     passenger_apache2 do
-      server_aliases deploy[:domains]
-      webapp_template "#{application}.conf.erb"
+      #server_aliases deploy[:domains]
+      #webapp_template "#{application}.conf.erb"
     end
   end
 end
