c8aacfa69fa29408844620025dd03f8aa17b874e
Rajesh Soni <rajesh.soni@saviance.com>
2014-05-29 23:35:32 +0530

    new recipe

--- catalog/recipes/deploy_test.rb
+++ catalog/recipes/deploy_test.rb
@@ -2,3 +2,16 @@ package 'htop' do
   package_name 'htop'
   action :install
 end
+template "/etc/log_files.yml" do
+  source "passenger.conf.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
+
+template "/etc/apache2/mods-available/passenger.load" do
+  source "passenger.load.erb"
+  owner "root"
+  group "root"
+  mode "644"
+end
