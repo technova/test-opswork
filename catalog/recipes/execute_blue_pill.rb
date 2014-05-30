c34c5c74589b3c26de382d2e1bf2b98ff2cbcb62
Rajesh Soni <rajesh.soni@saviance.com>
2014-05-30 16:10:36 +0530

    init commit

--- catalog/recipes/execute_blue_pill.rb
+++ catalog/recipes/execute_blue_pill.rb
@@ -1,7 +1,7 @@
 Chef::Log.info "Running bluepill..."
-# this code is crapy
+# this code is not working...
 command = "sudo bundle exec bluepill stop && sudo bundle exec bluepill load ./config/dj_bluepill.pill && sudo bundle exec bluepill start"
 execute command do
   cwd "/srv/www/goldappnew"
-  user 'deploy'
+  user 'root'
 end
