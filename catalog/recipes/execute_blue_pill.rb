5dca8700015529d369c71062a17dddff95b3ed0b
Rajesh Soni <rajesh.soni@saviance.com>
2014-05-30 15:53:09 +0530

    init commit

--- catalog/recipes/execute_blue_pill.rb
+++ catalog/recipes/execute_blue_pill.rb
@@ -1,4 +1,5 @@
 Chef::Log.info "Running bluepill..."
+# this code is crapy
 command = "sudo bundle exec bluepill stop && sudo bundle exec bluepill load ./config/dj_bluepill.pill && sudo bundle exec bluepill start"
 execute command do
   cwd "/srv/www/goldappnew"
