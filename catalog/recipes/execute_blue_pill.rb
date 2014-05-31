1744f3b4f7ad60ccd0c143ffcb3ff7bdec0fe758
Rajesh Soni <rajesh.soni@saviance.com>
2014-05-31 19:15:54 +0530

    more changes

--- catalog/recipes/execute_blue_pill.rb
+++ catalog/recipes/execute_blue_pill.rb
@@ -2,13 +2,15 @@ Chef::Log.info "Running bluepill..."
 # this code is not working...
 #
 # include_recipe "deploy"
-directory = "/srv/www/goldappnew/current"
+# directory = "/srv/www/goldappnew/current"
 commands = ["sudo bundle exec bluepill stop", "sudo bundle exec bluepill load ./config/dj_bluepill.pill", "sudo bundle exec bluepill start"]
-commands.each do |command|
-  execute "restart bluepill, running #{command}" do
-    cwd directory
-    command command
-    action :run    
-    not_if "ps awux | grep rake"
-  end    
+node[:deploy].each do |app, deploy|
+  commands.each do |command|
+    execute "restart bluepill, running #{command}" do
+      cwd "#{node[:deploy][app][:deploy_to]}/current"
+      command command
+      action :run
+      not_if "ps awux | grep rake"
+    end
+  end
 end
