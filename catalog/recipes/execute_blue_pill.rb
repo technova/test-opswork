f8b3c0e16407eb4e5a49adac568bb464c162cd56
Rajesh Soni <rajesh.soni@saviance.com>
2014-05-31 15:21:21 +0530

    more changes

--- catalog/recipes/execute_blue_pill.rb
+++ catalog/recipes/execute_blue_pill.rb
@@ -1,7 +1,7 @@
 Chef::Log.info "Running bluepill..."
 # this code is not working...
 @command = "sudo bundle exec bluepill stop && sudo bundle exec bluepill load ./config/dj_bluepill.pill && sudo bundle exec bluepill start"
-
+#
 # include_recipe "deploy"
 
 #node[:deploy].each do |application, deploy|
