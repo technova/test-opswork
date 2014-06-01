8f92d37328c0ce3e01547c627bd78455b7802d9b
Rajesh Soni <rajesh.soni@saviance.com>
2014-06-01 10:43:09 +0530

    more changes

--- catalog/recipes/deploy_test.rb
+++ catalog/recipes/deploy_test.rb
@@ -3,15 +3,24 @@ package 'htop' do
   action :install
 end
 template "/etc/log_files.yml" do
-  source "passenger.conf.erb"
+  source "log_files.yml.erb"
   owner "root"
   group "root"
   mode "644"
 end
 
-template "/etc/apache2/mods-available/passenger.load" do
-  source "passenger.load.erb"
+template "/etc/init.d/remote_syslog" do
+  source "remote_syslog.erb"
   owner "root"
   group "root"
-  mode "644"
+  mode "755"
+end
+
+service "remote_syslog" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+execute "remote_syslog -c /etc/log_files.yml --tls -p 32120" do
+  action :run
 end
